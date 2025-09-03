8b7b66b9b892a450c5bcfbba08983fb40c461249
Zoltan Hari <harizozi1009@gmail.com>
2025-09-03 10:03:16 -0600

    Fix Variable Numbers

--- fahrenheit-404.py
+++ fahrenheit-404.py
@@ -3,18 +3,18 @@ print("Option 1: Fahrenheit to Celsius")
 print("Option 2: Celsius to Fahrenheit")
 choice = input("Please select Option 1 or Option 2")
 if choice == "Option 1" or choice == "1":
-    temp = input("Please Enter the Temperature in Fahrenheit")
+    temp_fahr = input("Please Enter the Temperature in Fahrenheit")
     try:
-        num_temp = int(temp)
-        conv_temp = (num_temp - 32)/1.8
+        num_temp_fahr = int(temp_fahr)
+        conv_temp_fahr = (num_temp_fahr - 32)/1.8
     except Error:
         print("Invalid Input, Please Enter a Valid Number")
-    print(f"{temp}° in Celsius is {conv_temp}")
+    print(f"{temp_fahr}° in Celsius is {conv_temp_fahr}")
 elif choice == "Option 2" or choice == "2":
-    temp = input("Please Enter the Temperature in Celsius")
+    temp_cel = input("Please Enter the Temperature in Celsius")
     try:
-        num_temp = int(temp)
-        conv_temp = (num_temp * 1.8) + 32
+        num_temp_cel = int(temp_cel)
+        conv_temp_cel = (num_temp_cel * 1.8) + 32
     except Error:
         print("Invalid Input, Please Enter a Valid Number")
-    print(f"{temp}° in Fahrenheit is {conv_temp}")
+    print(f"{temp_cel}° in Fahrenheit is {conv_temp_cel}")
