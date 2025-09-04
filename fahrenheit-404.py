209eec46c6483137e1e6ae5119456a12bfba662a
Zoltan Hari <harizozi1009@gmail.com>
2025-09-04 07:10:53 -0600

    Round Temp's to 1 Decimal

--- fahrenheit-404.py
+++ fahrenheit-404.py
@@ -6,16 +6,18 @@ while True:
     if choice == "Option 1" or choice == "1":
         temp_fahr = input("Please Enter the Temperature in Fahrenheit\n")
         try:
-            num_temp_fahr = int(temp_fahr)
-            conv_temp_fahr = (num_temp_fahr - 32)/1.8
+            float_temp_fahr = float(temp_fahr)
+            conv_temp_fahr = (float_temp_fahr - 32)/1.8
+            fin_temp_fahr = round(conv_temp_fahr, 1)
         except:
             print("Invalid Input, Please Enter a Valid Number")
-        print(f"{temp_fahr}° in Celsius is {conv_temp_fahr}")
+        print(f"{temp_fahr}° in Celsius is {fin_temp_fahr}")
     elif choice == "Option 2" or choice == "2":
         temp_cel = input("Please Enter the Temperature in Celsius\n")
         try:
-            num_temp_cel = int(temp_cel)
-            conv_temp_cel = (num_temp_cel * 1.8) + 32
+            float_temp_cel = float(temp_cel)
+            conv_temp_cel = (float_temp_cel * 1.8) + 32
+            fin_temp_cel = round(fin_temp_cel, 1)
         except:
             print("Invalid Input, Please Enter a Valid Number")
         print(f"{temp_cel}° in Fahrenheit is {conv_temp_cel}")
